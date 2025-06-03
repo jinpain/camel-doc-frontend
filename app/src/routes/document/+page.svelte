8bc93d23e342e9db81929b08a1e144c87339dbf1
jinpain <savpavel2000@gmail.com>
2025-06-03 17:04:42 +0500

    doc page

---
+++ app/src/routes/document/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+</script>
+
+<ul class="nav nav-tabs" id="docTab" role="tablist">
+	<li class="nav-item" role="presentation">
+		<button
+			class="nav-link active"
+			id="details-tab"
+			data-bs-toggle="tab"
+			data-bs-target="#details-tab-pane">Реквизиты</button
+		>
+	</li>
+	<li class="nav-item" role="presentation">
+		<button
+			class="nav-link"
+			id="attachments-tab"
+			data-bs-toggle="tab"
+			data-bs-target="#attachments-tab-pane">Вложения</button
+		>
+	</li>
+</ul>
+<div class="tab-content" id="docTabContent">
+	<div class="tab-pane fade show active" id="details-tab-pane">
+        <div class="form-floating mb-3 w-25">
+            <input class="form-control" id="floatingInput">
+        </div>
+	</div>
+	<div class="tab-pane fade" id="attachments-tab-pane">
+		<button class="btn btn-sm btn-secondary">Добавить</button>
+		<table class="table table-sm table-hover">
+			<thead>
+				<tr>
+					<th>Важное</th>
+					<th>Наименование</th>
+					<th>Дата создания</th>
+					<th>Автор</th>
+				</tr>
+			</thead>
+		</table>
+	</div>
+</div>
---
+++ app/src/routes/document/[id]/+page.svelte
@@ -0,0 +1,60 @@
+<script>
+	export let data;
+	console.log(data.id)
+</script>
+
+<ul class="nav nav-tabs" id="docTab" role="tablist">
+	<li class="nav-item" role="presentation">
+		<button
+			class="nav-link active"
+			id="details-tab"
+			data-bs-toggle="tab"
+			data-bs-target="#details-tab-pane"
+			type="button"
+			role="tab"
+			aria-controls="details-tab-pane"
+			aria-selected="true">Реквизиты</button
+		>
+	</li>
+	<li class="nav-item" role="presentation">
+		<button
+			class="nav-link"
+			id="attachments-tab"
+			data-bs-toggle="tab"
+			data-bs-target="#attachments-tab-pane"
+			type="button"
+			role="tab"
+			aria-controls="attachments-tab-pane"
+			aria-selected="false">Вложения</button
+		>
+	</li>
+</ul>
+<div class="tab-content" id="docTabContent">
+	<div
+		class="tab-pane fade show active"
+		id="details-tab-pane"
+		role="tabpanel"
+		aria-labelledby="details-tab"
+		tabindex="0"
+	></div>
+	<div
+		class="tab-pane fade"
+		id="attachments-tab-pane"
+		role="tabpanel"
+		aria-labelledby="attachments-tab"
+		tabindex="0"
+	>
+    <button class="btn btn-sm btn-secondary">Добавить</button>
+ 
+    <table class="table table-sm table-hover">
+        <thead>
+            <tr>
+                <th>Важное</th>
+                <th>Наименование</th>
+                <th>Дата создания</th>
+                <th>Автор</th>
+            </tr>
+        </thead>
+    </table>
+	</div>
+</div>
